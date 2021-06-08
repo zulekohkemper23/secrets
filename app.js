27bf31a606aac117af8826a241976fac779672a1
zuleyka kohkemper <zulecr23@hotmail.comgit config --global user.email zulecr23@hotmail.comgit config --global user.email zulecr23@hotmail.com>
2021-06-07 22:48:21 -0600

    pass hash

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -26,10 +26,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, {
-  secret:process.env.SECRET,
-  encryptedFields: ["password"]
-});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -50,7 +46,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err) {
     if (err) {
@@ -63,7 +59,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const pass = req.body.password;
+  const pass = md5(req.body.password);
 
   User.findOne({
     email: username
