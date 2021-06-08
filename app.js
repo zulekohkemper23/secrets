bcf3aeb641bf4c222f165441d26002747b8b7927
zuleyka kohkemper <zulecr23@hotmail.comgit config --global user.email zulecr23@hotmail.comgit config --global user.email zulecr23@hotmail.com>
2021-06-07 23:30:32 -0600

    bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -44,22 +45,25 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const pass = md5(req.body.password);
+  const pass = req.body.password;
 
   User.findOne({
     email: username
@@ -68,13 +72,18 @@ app.post("/login", function(req, res) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === pass) {
-          res.render("secrets");
-        }
+        bcrypt.compare(pass, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
       }
     }
   });
 });
+
+
+
 
 
 app.listen(3000, function() {
